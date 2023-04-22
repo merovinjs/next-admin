d9d5be986fc8dd818a6a8e2aad076ee49ad2234f
Proprogam <k.kartal9595@gmail.com>
2023-04-22 19:32:33 +0300

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,8 @@
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
-import clientPromise from "../../../../lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
 import GitHubProvider from "next-auth/providers/github";
-const handler = NextAuth({
+export default NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
@@ -12,4 +12,3 @@ const handler = NextAuth({
   secret: process.env.NEXT_AUTH_SECRET,
   adapter: MongoDBAdapter(clientPromise),
 });
-export { handler as GET, handler as POST };
--- pages/loggin.jsx
+++ pages/loggin.jsx
@@ -1,9 +1,6 @@
-import { Inter } from "next/font/google";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import styles from "./styles.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const router = useRouter();
